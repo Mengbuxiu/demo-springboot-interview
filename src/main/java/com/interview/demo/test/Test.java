e4dea1c3640bd3a6be43464d1cca5e4c523d9706
zlmi <15910344021@163.com>
2019-01-17 09:17:56 +0800

    demo尝试

---
+++ src/main/java/com/interview/demo/test/Test.java
@@ -0,0 +1,41 @@
+package com.interview.demo.test;
+
+
+import com.alibaba.fastjson.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test {
+    public static void main(String[] args) {
+//        JSONObject.fromObject("{\"level\":[\"3\"]}");
+
+
+        //Object -> Integer || Integer -> Object   都会报编译错误
+//        List<Object> intList = new ArrayList<>(3);
+//        intList.add(111);
+//        List<?> objects = intList;
+    }
+//    @Test
+//    public void maopao() {
+//        boolean swapped;
+//        int[] arr = new int[]{5, 2, 6, 7, 3};
+//        do {
+//             swapped = false;
+//            for (int i = 0; i < arr.length - 1; i++) {
+//                if (arr[i] < arr[i + 1]){
+//                    int temp = arr[i];
+//                    arr[i] = arr[i + 1];
+//                    arr[i + 1] = temp;
+//                    swapped = true;
+//                }
+//            }
+//        }while (swapped);
+//        for (int a:arr) {
+//            System.out.print(a +" ");
+//        }
+//    }
+
+
+
+}
