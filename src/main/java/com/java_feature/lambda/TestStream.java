d7eba8828a8567e0658f55d5ae6f107df3c1455a
zlmi <15910344021@163.com>
2019-04-10 13:08:10 +0800

    lambda集合stream

---
+++ src/main/java/com/java_feature/lambda/TestStream.java
@@ -0,0 +1,18 @@
+package com.java_feature.lambda;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author king_zl
+ * @version 1.0
+ * @description // lambda集合stream
+ * @date 2019/4/10 12:53
+ */
+public class TestStream {
+    public static void main(String[] args) {
+        List<String> list = Arrays.asList("zheng", "lin");
+        //list.stream().map(item -> item.toUpperCase()).forEach(System.out::println);
+        list.stream().map(String::toUpperCase).forEach(System.out::println);
+    }
+}
