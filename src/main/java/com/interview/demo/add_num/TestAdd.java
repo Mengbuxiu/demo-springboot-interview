21fe12d4dc00803284c867696a16ea010772e440
zlmi <15910344021@163.com>
2019-04-24 18:30:07 +0800

    think in java 迭代器设计模式

---
+++ src/main/java/com/interview/demo/add_num/TestAdd.java
@@ -0,0 +1,14 @@
+package com.interview.demo.add_num;
+
+/**
+ * @author king_zl
+ * @version 1.0
+ * @description // TODO
+ * @date 2019/4/24 16:23
+ */
+public class TestAdd {
+    public static void main(String[] args) {
+        int i = -2;
+        System.out.println(1+ i);
+    }
+}
---
+++ src/main/java/com/thinkinjava/basic/_10_3/DotNew.java
@@ -0,0 +1,21 @@
+package com.thinkinjava.basic._10_3;
+
+/**
+ * @author king_zl
+ * @version 1.0
+ * @description // TODO
+ * @date 2019/4/24 17:49
+ */
+public class DotNew {
+    public class Inner{
+        void p(){
+            System.out.println("I'm fine");
+        }
+    }
+
+    public static void main(String[] args) {
+//        DotNew dn = new DotNew();
+        Inner i = new DotNew().new Inner();
+        i.p();
+    }
+}
