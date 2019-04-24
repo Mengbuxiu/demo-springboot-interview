015a0263edc3f560fe11f1db3ca0bfdca1c02057
Alin <15910344021@163.com>
2019-04-24 08:14:05 +0800

    学习内部类用法

---
+++ src/main/java/com/thinkinjava/basic/_10_1/Parcel1.java
@@ -0,0 +1,33 @@
+package com.thinkinjava.basic._10_1;
+
+import sun.security.krb5.internal.crypto.Des;
+
+/**
+ * @author Administrator
+ * @desc //创建内部类
+ * @date 2019/4/23
+ */
+public class Parcel1 {
+    class Contents{
+        private int i = 11;
+        public int value(){ return i; }
+    }
+    class Destination{
+        private String label;
+        Destination(String whereTo) {
+            label = whereTo;
+        }
+        String readLabel() { return label; }
+    }
+    public void ship(String dest){
+        Contents c = new Contents();
+        System.out.println(c.value());
+        Destination d = new Destination(dest);
+        System.out.println(d.readLabel());
+    }
+
+    public static void main(String[] args) {
+        Parcel1 p = new Parcel1();
+        p.ship("beijing");
+    }
+}
