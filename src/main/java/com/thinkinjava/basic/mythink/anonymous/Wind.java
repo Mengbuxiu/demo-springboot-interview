d6ead234b4c363ac5e1972173280370809b06e50
zlmi <15910344021@163.com>
2019-04-26 15:01:37 +0800

    think in java 内部类一点思考

---
+++ src/main/java/com/thinkinjava/basic/mythink/anonymous/Wind.java
@@ -0,0 +1,14 @@
+package com.thinkinjava.basic.mythink.anonymous;
+
+/**
+ * @author Alin
+ * @version 1.0
+ * @description // 抽象类实现接口时可选择对接口中的固有方法是否重写
+ * @date 2019/4/26 14:59
+ */
+public abstract class Wind implements Instrument{
+//    @Override
+//    public void play() {
+//
+//    }
+}
