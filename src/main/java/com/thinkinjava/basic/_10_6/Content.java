c9e06b935df3fb32ea8660560f6b0e713eff2674
zlmi <15910344021@163.com>
2019-04-26 18:39:51 +0800

    think in java 内部类实践

---
+++ src/main/java/com/thinkinjava/basic/_10_6/Content.java
@@ -0,0 +1,11 @@
+package com.thinkinjava.basic._10_6;
+
+/**
+ * @author Alin
+ * @version 1.0
+ * @description // just a interface for ContentImpl
+ * @date 2019/4/26 14:06
+ */
+public interface Content {
+    Integer getValue();
+}
---
+++ src/main/java/com/thinkinjava/basic/_10_6/Wrapping.java
@@ -0,0 +1,18 @@
+package com.thinkinjava.basic._10_6;
+
+/**
+ * @author Alin
+ * @version 1.0
+ * @description // TODO
+ * @date 2019/4/26 14:32
+ */
+public class Wrapping {
+    private int x;
+    public Wrapping(int x) {
+        this.x = x;
+    }
+
+    protected int val() {
+        return this.x;
+    }
+}
