fb7f0975774911cea730dad2d7a94dbb3b67c0b4
zlmi <15910344021@163.com>
2019-04-30 16:16:05 +0800

    think in java 接口中的方法可以不在抽象类中实现

--- src/main/java/com/thinkinjava/basic/mythink/anonymous/Wind.java
+++ src/main/java/com/thinkinjava/basic/mythink/anonymous/Wind.java
@@ -5,6 +5,7 @@ package com.thinkinjava.basic.mythink.anonymous;
  * @version 1.0
  * @description // 抽象类实现接口时可选择对接口中的固有方法是否重写
  * @date 2019/4/26 14:59
+ * 如下，注释掉了接口中的方法也不会报错，so，可以不实现
  */
 public abstract class Wind implements Instrument{
 //    @Override
