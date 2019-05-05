7fe64d6fd7e7963f8a2ca523a6961d7868815e4e
zlmi <15910344021@163.com>
2019-05-05 17:32:13 +0800

    think in java java 泛型和静态内部类的一种应用-随机产出咖啡

---
+++ src/main/java/com/thinkinjava/basic/_15_3/Cappuccino.java
@@ -0,0 +1,10 @@
+package com.thinkinjava.basic._15_3;
+
+/**
+ * @author Alin
+ * @version 1.0
+ * @description // TODO
+ * @date 2019/5/5 14:08
+ */
+public class Cappuccino extends Coffee {
+}
---
+++ src/main/java/com/thinkinjava/basic/_15_3/Generator.java
@@ -0,0 +1,11 @@
+package com.thinkinjava.basic._15_3;
+
+/**
+ * @author Alin
+ * @version 1.0
+ * @description // 生成器接口，用于产生新的对象
+ * @date 2019/5/5 14:02
+ */
+public interface Generator<T> {
+    T next();
+}
