b83b7972ae793004755cc3d189240498f3f15bd1
zlmi <15910344021@163.com>
2019-05-06 16:46:26 +0800

    think in java java代理模式

--- src/main/java/com/thinkinjava/basic/_14_7/package-info.java
+++ src/main/java/com/thinkinjava/basic/_14_7/package-info.java
@@ -1,6 +1,8 @@
 package com.thinkinjava.basic._14_7;
 
 /**
+ * 这篇文章讲的很好
+ * http://www.cnblogs.com/silverLee/archive/2010/02/05/1664577.html
  * 1.用插入的对象来代替实际的对象，代理通常充当中间人的角色
  * 2.何时用代理？ 将额外的操作从实际的对象中分离到不同的地方，使用简单的
  * 操作以做出适合实际应用的修改
