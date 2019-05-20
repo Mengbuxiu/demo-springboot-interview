b99ba48e54ab04392171aa95bd7ace014f2a5c6d
zlmi <15910344021@163.com>
2019-05-20 10:51:21 +0800

    java-style

--- src/main/java/com/interview/demo/RmiTest/HelloClient.java
+++ src/main/java/com/interview/demo/RmiTest/HelloClient.java
@@ -10,7 +10,7 @@ import java.rmi.RemoteException;
 * 客户端测试，在客户端调用远程对象上的远程方法，并返回结果。
 */ 
 public class HelloClient { 
-    public static void main(String args[]){ 
+    public static void main(String[] args){
         try { 
             //在RMI服务注册表中查找名称为RHello的对象，并调用其上的方法 
             IHello rhello =(IHello) Naming.lookup("rmi://localhost:8888/RHello");
