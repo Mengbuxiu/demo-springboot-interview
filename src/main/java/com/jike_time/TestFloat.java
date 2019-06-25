f63503a42d7a1f29b8127b6353fa3ccb71f64454
zlmi <15910344021@163.com>
2019-06-25 18:10:54 +0800

    true

---
+++ src/main/java/com/jike_time/TestFloat.java
@@ -0,0 +1,18 @@
+package com.jike_time;
+
+/**
+ * @author Alin
+ * @version 1.0
+ * @description // TODO
+ * @date 2019/6/25 18:06
+ */
+public class TestFloat {
+    public static void main(String[] args) {
+        float a = 1.0f - 0.9f;
+        float b = 0.6f - 0.5f;
+        if (a == b) {
+            System.out.println(true);
+        }
+        System.out.println(false);
+    }
+}
