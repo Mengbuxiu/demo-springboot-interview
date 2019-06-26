b63d10847a8206c89096014269fdebb85d6d00c6
zlmi <15910344021@163.com>
2019-06-26 16:28:51 +0800

    nsq

---
+++ src/main/java/com/interview/demo/nsq/NSQTest.java
@@ -0,0 +1,17 @@
+package com.interview.demo.nsq;
+
+import com.github.brainlag.nsq.NSQMessage;
+import com.github.brainlag.nsq.callbacks.NSQMessageCallback;
+
+/**
+ * @author Alin
+ * @version 1.0
+ * @description // TODO
+ * @date 2019/6/26 14:02
+ */
+public class NSQTest implements NSQMessageCallback {
+    @Override
+    public void message(NSQMessage nsqMessage) {
+
+    }
+}
