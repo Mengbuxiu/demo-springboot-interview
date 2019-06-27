2cc828c1d7e9c182623da0c57bbdbc99fbb596db
zlmi <15910344021@163.com>
2019-06-27 18:04:38 +0800

    并发模拟

---
+++ src/main/java/com/jike_time/juc/SafeWM.java
@@ -0,0 +1,53 @@
+package com.jike_time.juc;
+
+import lombok.ToString;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+/**
+ * @author Alin
+ * @version 1.0
+ * @description // 库存静态
+ * @date 2019/6/27 11:46
+ */
+@ToString
+@Slf4j
+public class SafeWM {
+    /**
+     * 库存上限
+     */
+    private final AtomicLong upper = new AtomicLong(10);
+    /**
+     * 库存下限
+     */
+    private final AtomicLong lower = new AtomicLong(0);
+
+    void setUpper(long v) {
+        // 看见if想竞态条件
+        if (v < lower.get()) {
+            System.out.println(lower.get() + " : " + upper.get());
+            log.error(String.format("库存上限(%d)小于下限(%d)   %d < %d",v,lower.get(),v,lower.get()));
+            return;
+        }
+        upper.set(v);
+        System.out.println("------------after update upper------------");
+        System.out.println(lower.get() + " : " + upper.get());
+    }
+    void setLowwer(long v){
+        if (v > upper.get()){
+            System.out.println(lower.get() + " : " + upper.get());
+            log.error(String.format("库存下限(%d)大于上限(%d) %d > %d",v,upper.get(),v,upper.get()));
+            return;
+        }
+        lower.set(v);
+        System.out.println("------------after update lower------------");
+        System.out.println(lower.get() + " : " + upper.get());
+    }
+    long getUpper(){
+        return this.upper.get();
+    }
+    long getLower(){
+        return this.lower.get();
+    }
+}
---
+++ src/main/java/com/jike_time/juc/TestSafe.java
@@ -0,0 +1,43 @@
+package com.jike_time.juc;
+
+
+import java.util.concurrent.CountDownLatch;
+
+/**
+ * @author Alin
+ * @version 1.0
+ * @description // TODO
+ * @date 2019/6/27 11:53
+ */
+public class TestSafe {
+    public static void main(String[] args) {
+        SafeWM safe = new SafeWM();
+        safe.setLowwer(2);
+        safe.setUpper(10);
+        CountDownLatch latch = new CountDownLatch(2);
+        Thread thread1 = new Thread(() -> {
+            try {
+                latch.await();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            safe.setLowwer(7);
+        });
+        Thread thread2 = new Thread(() -> {
+            try {
+                latch.await();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            safe.setUpper(5);
+        });
+
+        thread1.start();
+        latch.countDown();
+        thread2.start();
+        latch.countDown();
+        if (safe.getLower() > safe.getUpper()) {
+            System.out.println("------------->  " + safe);
+        }
+    }
+}
