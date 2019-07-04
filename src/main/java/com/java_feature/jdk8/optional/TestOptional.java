5f6cc0ec0a785ccae04e96a8e5d8ee5bd6798f4c
zlmi <15910344021@163.com>
2019-07-04 17:23:42 +0800

    spring Datasize

--- src/main/java/com/java_feature/jdk8/optional/TestOptional.java
+++ src/main/java/com/java_feature/jdk8/optional/TestOptional.java
@@ -3,6 +3,9 @@ package com.java_feature.jdk8.optional;
 import com.java_feature.tenum.entity.User;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.convert.DataSizeUnit;
+import org.springframework.util.unit.DataSize;
+import org.springframework.util.unit.DataUnit;
 
 import java.util.Optional;
 
@@ -23,5 +26,16 @@ class TestOptional {
         System.out.println("age = " + age);
         System.out.println(isGirl);
         System.out.println(isGirl1);
+
+    }
+    @DataSizeUnit(DataUnit.MEGABYTES)
+    private DataSize dataSize1 = DataSize.ofMegabytes(2);
+    @Test
+    void testSize(){
+        // 输出结果都是byte，除非@DataSizeUnit指定
+        DataSize dataSize = DataSize.ofMegabytes(1);
+
+        System.out.println(Integer.valueOf(dataSize.toString().replace("B",""))/1024);
+        System.out.println(Integer.valueOf(dataSize1.toString().replace("B",""))/1024);
     }
 }
