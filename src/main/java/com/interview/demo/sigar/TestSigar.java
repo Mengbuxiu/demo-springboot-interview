8fc985deadaab9a9e6f80e301803e97a7e467f1e
zlmi <15910344021@163.com>
2019-08-09 09:50:46 +0800

    16yi

--- src/main/java/com/interview/demo/sigar/TestSigar.java
+++ src/main/java/com/interview/demo/sigar/TestSigar.java
@@ -11,11 +11,28 @@ import org.junit.jupiter.api.Test;
  * @description // TODO
  * @date 2019/7/9 16:22
  */
-public class TestSigar {
+class TestSigar {
     @Test
     void testSigar() throws SigarException {
         Sigar sigar = new Sigar();
         NetInterfaceConfig cfg = sigar.getNetInterfaceConfig();
         System.out.println(cfg.getAddress());
+    }
+
+    @Test
+    void testCPU() {
+        long startTime = System.currentTimeMillis();
+        long endTime = startTime + 6000;
+        long index = 0;
+        while (true) {
+            double x = Math.sqrt(index);
+            long now = System.currentTimeMillis();
+            if (now > endTime) {
+                break;
+            }
+            index++;
+        }
+        System.out.println(index + " Loops in 6 seconds");
+
     }
 }
