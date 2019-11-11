2d035e969ea41e41c8736192aaadab136de9e9bb
zlmi <15910344021@163.com>
2019-11-11 16:16:32 +0800

    文件下载demo

---
+++ src/main/java/com/interview/demo/filedownload/FileDownloadController.java
@@ -0,0 +1,41 @@
+package com.interview.demo.filedownload;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
+
+/**
+ * @author Alin
+ * @desc 下载图片
+ * @date 2019/5/4
+ */
+@Controller
+@RequestMapping("/file")
+public class FileDownloadController {
+    @RequestMapping("/down")
+    public void upload(HttpServletResponse rs) {
+        rs.reset();
+        rs.setHeader("Content-disposition", "attachment; filename=test.jpg");
+        rs.encodeRedirectURL("UTF-8");
+        rs.setContentType("application/octet-stream");
+        final ServletOutputStream out;
+        try {
+            out = rs.getOutputStream();
+            final BufferedInputStream bis = new BufferedInputStream(new FileInputStream(new File("C:\\Users\\king_zl\\Pictures\\Saved Pictures\\3.jpg")));
+            byte[] buff = new byte[bis.available()];
+            bis.read(buff);
+            out.write(buff);
+            out.flush();
+            out.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
