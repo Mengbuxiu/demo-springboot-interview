14eed3aaa43cf3b0d1bb96e135c892f3d50c31de
zlmi <15910344021@163.com>
2020-01-19 10:15:20 +0800

    DateTimeFormatter

--- src/test/java/com/interview/demo/util/DateUtilTest.java
+++ src/test/java/com/interview/demo/util/DateUtilTest.java
@@ -3,8 +3,9 @@ package com.interview.demo.util;
 import org.junit.jupiter.api.Test;
 
 import java.time.*;
+import java.time.format.DateTimeFormatter;
 import java.time.format.TextStyle;
-import java.time.temporal.TemporalAdjuster;
+import java.time.temporal.ChronoUnit;
 import java.time.temporal.TemporalAdjusters;
 import java.util.*;
 
@@ -32,8 +33,82 @@ import java.util.*;
  *      <li> ZoneId指定时区标识符，并提供在Instant和LocalDateTime之间进行转换的规则。</li>
  *      <li> ZoneOffset指定与格林威治/UTC时间的时区偏移量。</li>
  *  </ul>
+ *  <p>Date-Time API提供了三个与时区一起使用的基于时间的类：</p>
+ *  <ul>
+ *      <li>ZonedDateTime处理具有相应时区的日期和时间，其时区偏离格林威治/ UTC。</li>
+ *      <li>OffsetDateTime处理具有与格林威治/ UTC相对应的时区偏移的日期和时间，而没有时区ID。</li>
+ *      <li>OffsetTime处理时间与格林威治/ UTC有相应时区偏移的时间，而没有时区ID。</li>
+ *  </ul>
  */
 class DateUtilTest {
+    @Test
+    void dateTimeFormatter() {
+        //parsing
+        String in = "20200120";
+//        String in = String.valueOf(DateUtil.localDate2Millis(LocalDate.now()));
+        LocalDate date = LocalDate.parse(in, DateTimeFormatter.BASIC_ISO_DATE);
+        System.out.println(date);
+    }
+    @Test
+    void instant() {
+        System.out.printf("Instant now: %s\n", Instant.now());
+        System.out.printf("LocalDateTime now: %s\n", LocalDateTime.now());
+
+        final Instant epoch = Instant.ofEpochSecond(0L);
+        System.out.println("epoch = " + epoch);
+        long secondsFromEpoch = epoch.until(Instant.now(),
+                ChronoUnit.SECONDS);
+        System.out.println("secondsFromEpoch = " + secondsFromEpoch);
+    }
+    @Test
+    void offsetDateTime() {
+        // Find the last Thursday in JANUARY 2020.
+        LocalDateTime localDate = LocalDateTime.of(2020, Month.JANUARY, 19, 9, 1);
+        ZoneOffset offset = ZoneOffset.of("-08:00");
+
+        OffsetDateTime offsetDate = OffsetDateTime.of(localDate, offset);
+        OffsetDateTime lastThursday =
+                offsetDate.with(TemporalAdjusters.lastInMonth(DayOfWeek.THURSDAY));
+        System.out.printf("The last Thursday in JANUARY 2020 is the %sth.%n",
+                lastThursday.getDayOfMonth());
+    }
+    @Test
+    void zoneDateTime() {
+        DateTimeFormatter format = DateTimeFormatter.ofPattern("MMM d yyyy  hh:mm a");
+
+// Leaving from San Francisco on July 20, 2013, at 7:30 p.m.
+        LocalDateTime leaving = LocalDateTime.of(2013, Month.JULY, 20, 19, 30);
+        ZoneId leavingZone = ZoneId.of("America/Los_Angeles");
+        ZonedDateTime departure = ZonedDateTime.of(leaving, leavingZone);
+
+        try {
+            String out1 = departure.format(format);
+            System.out.printf("LEAVING:  %s (%s)%n", out1, leavingZone);
+        } catch (DateTimeException exc) {
+            System.out.printf("%s can't be formatted!%n", departure);
+            throw exc;
+        }
+
+        // Flight is 10 hours and 50 minutes, or 650 minutes
+        ZoneId arrivingZone = ZoneId.of("Asia/Tokyo");
+        ZonedDateTime arrival = departure.withZoneSameInstant(arrivingZone)
+                .plusMinutes(650);
+
+        try {
+            String out2 = arrival.format(format);
+            System.out.printf("ARRIVING: %s (%s)%n", out2, arrivingZone);
+        } catch (DateTimeException exc) {
+            System.out.printf("%s can't be formatted!%n", arrival);
+            throw exc;
+        }
+
+        if (arrivingZone.getRules().isDaylightSavings(arrival.toInstant()))
+            System.out.printf("  (%s daylight saving time will be in effect.)%n",
+                    arrivingZone);
+        else
+            System.out.printf("  (%s standard time will be in effect.)%n",
+                    arrivingZone);
+    }
     /**
      * 格林威治时间/ UTC时间的偏移量通常以小时为单位进行定义，但也有例外。
      * TimeZoneId示例中的以下代码 显示使用格林威治标准时间/ UTC的偏移量（未在整小时内定义）的所有时区的列表。
