887d239bbe34d4b9c1d190699e7cc2bce8eaffc1
Alin <15910344021@163.com>
2020-04-05 12:32:59 +0800

    子线程任务完成回调通知主线程

---
+++ src/main/java/com/thinkinjava/advanced/_21_2/_4/FindMaxTask.java
@@ -0,0 +1,35 @@
+package com.thinkinjava.advanced._21_2._4;
+
+import java.util.concurrent.Callable;
+
+/**
+ * @author Alin
+ * @desc // 子线程执行完毕后回调主线程，通知执行完毕
+ * @date 2020/4/5
+ */
+public class FindMaxTask implements Callable<Integer> {
+    private int[] data;
+    private int start;
+    private int end;
+
+    public FindMaxTask() {
+    }
+
+    public FindMaxTask(int[] data, int start, int end) {
+        this.data = data;
+        this.start = start;
+        this.end = end;
+    }
+
+    @Override
+    public Integer call() {
+        // find the max number
+        int max = Integer.MIN_VALUE;
+        for (int i = start; i < end; i++) {
+            if (data[i] > max) {
+                max = data[i];
+            }
+        }
+        return max;
+    }
+}
