c12f299d25386f410ae136880279f95c7f137690
lin <woooodlin@gmail.com>
2020-05-21 09:41:30 +0800

    happens-before start

---
+++ src/main/java/com/jeek_time/java并发编程实战/二/happensbefore/TestStart.java
@@ -0,0 +1,22 @@
+package com.jeek_time.java并发编程实战.二.happensbefore;
+
+/**
+ * 测试happens-before中的start规则
+ * 主线程中start子线程B，在start B线程时可看到主线程对共享变量的操作， 0 -> 1
+ */
+class TestStart implements Runnable{
+    static int i=0;
+    public static void main(String[] args) {
+        i=1;
+        final Thread subThreadB = new Thread(new TestStart());
+        subThreadB.setName("子线程B");
+
+        subThreadB.start();
+    }
+
+    @Override
+    public void run() {
+        System.out.println(TestStart.i);
+    }
+}
+
