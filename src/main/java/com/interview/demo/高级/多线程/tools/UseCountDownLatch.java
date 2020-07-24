756c032aeea003c6a987b5e0525592248e0342b4
lin <woooodlin@gmail.com>
2020-07-24 18:09:33 +0800

    change

--- src/main/java/com/interview/demo/高级/多线程/tools/UseCountDownLatch.java
+++ src/main/java/com/interview/demo/高级/多线程/tools/UseCountDownLatch.java
@@ -20,10 +20,8 @@ public class UseCountDownLatch {
         	System.out.println("Init Thread_"+Thread.currentThread().getId()
         			+" ready init work......");
         	latch.countDown();//初始化线程完成工作了，countDown方法只扣减一次；
-            for(int i =0;i<2;i++) {
             	System.out.println("\tThread_"+Thread.currentThread().getId()
             			+" ........continue do its work");
-            }
         }
     }
     
@@ -37,26 +35,24 @@ public class UseCountDownLatch {
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
-            for(int i =0;i<3;i++) {
             	System.out.println("Busi Thread_"+Thread.currentThread().getId()
             			+" do business-----");
-            }
         }
     }
 
     public static void main(String[] args) throws InterruptedException {
     	//单独的初始化线程,初始化分为2步，需要扣减两次
-        /*new Thread(() -> {
-            SleepTools.ms(1);
-            System.out.println("Thread_"+Thread.currentThread().getId()
-                    +" ready init work step 1st......");
-            latch.countDown();//每完成一步初始化工作，扣减一次
-            System.out.println("begin step 2nd.......");
-            SleepTools.ms(1);
-            System.out.println("Thread_"+Thread.currentThread().getId()
-                    +" ready init work step 2nd......");
-            latch.countDown();//每完成一步初始化工作，扣减一次
-        }).start();*/
+//        new Thread(() -> {
+//            SleepTools.ms(1);
+//            System.out.println("Thread_"+Thread.currentThread().getId()
+//                    +" ready init work step 1st......");
+//            latch.countDown();//每完成一步初始化工作，扣减一次
+//            System.out.println("begin step 2nd.......");
+//            SleepTools.ms(1);
+//            System.out.println("Thread_"+Thread.currentThread().getId()
+//                    +" ready init work step 2nd......");
+//            latch.countDown();//每完成一步初始化工作，扣减一次
+//        }).start();
 
         new Thread(new BusiThread()).start();
 
