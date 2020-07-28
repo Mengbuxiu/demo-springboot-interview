e87836965f6e2ece27158888193379fc29cde981
Alin <15910344021@163.com>
2020-07-28 08:02:13 +0800

    aqs，模板方法，condition

---
+++ src/main/java/com/interview/demo/高级/多线程/template/SendSms.java
@@ -0,0 +1,38 @@
+package com.interview.demo.高级.多线程.template;
+
+/**
+ * @author Mark
+ *模板方法的派生类
+ */
+public class SendSms extends SendCustom {
+
+	@Override
+	public void to() {
+		System.out.println("Mark");
+
+	}
+
+	@Override
+	public void from() {
+		System.out.println("Bill");
+
+	}
+
+	@Override
+	public void content() {
+		System.out.println("Hello world");
+
+	}
+
+	@Override
+	public void send() {
+		System.out.println("Send sms");
+
+	}
+	
+	public static void main(String[] args) {
+		SendCustom sendC = new SendSms();
+		sendC.sendMessage();
+	}
+
+}
