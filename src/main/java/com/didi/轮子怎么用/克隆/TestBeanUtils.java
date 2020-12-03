c20b6bf644b2c1f9aa72e9f4e56509658dd216e1
zhenglin <linzhenglin_v@xiaojuchefu.com>
2020-12-03 20:30:13 +0800

    clone

---
+++ src/main/java/com/didi/轮子怎么用/克隆/TestBeanUtils.java
@@ -0,0 +1,23 @@
+package com.didi.轮子怎么用.克隆;
+
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.beans.BeanUtils;
+
+public class TestBeanUtils {
+    public static void main(String[] args) {
+        Person src = Person.builder().age(11).name("zl").build();
+        Person target = new Person();
+        BeanUtils.copyProperties(src, target);
+        System.out.println(target);
+    }
+
+    @Data
+    @NoArgsConstructor
+    @Builder
+    private static class Person {
+        private String name;
+        private Integer age;
+    }
+}
