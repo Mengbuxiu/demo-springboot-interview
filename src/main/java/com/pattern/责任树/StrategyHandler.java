d7ab147bf44e8e3e54281db3d294be8ec490f25b
Alin <15910344021@163.com>
2021-02-22 22:45:45 +0800

    注释

--- src/main/java/com/pattern/责任树/StrategyHandler.java
+++ src/main/java/com/pattern/责任树/StrategyHandler.java
@@ -6,7 +6,7 @@ package com.pattern.责任树;
  * 中实现业务逻辑即可。对于其他责任树中的中间层节点，都需要同时继承 Router 抽象类和实现 Handler 接口，
  * 在 R apply(T param); 方法中首先进行一定异常入参拦截，遵循 fail-fast 原则，避免将这一层可以拦截的错误传递到下一层，
  * 同时也要避免“越权”做非本层职责的拦截校验，避免产生耦合，为后面业务拓展挖坑。
- * 在拦截逻辑后直接调用本身 Router 的 public R applyStrategy(T param) 方法路由给下游节点即可。
+ * 在拦截逻辑后直接调用本身 {@link AbstractStrategyRouter} 的 public R applyStrategy(T param) 方法路由给下游节点即可。
  */
 public interface StrategyHandler<T, R> {
     @SuppressWarnings("rawtypes")
