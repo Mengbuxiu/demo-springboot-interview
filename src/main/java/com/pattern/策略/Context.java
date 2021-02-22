2cc6e72290b1fe1e48a0e753e418374dae53b172
Alin <15910344021@163.com>
2021-02-22 22:29:07 +0800

    注释

--- src/main/java/com/pattern/策略/Context.java
+++ src/main/java/com/pattern/策略/Context.java
@@ -1,5 +1,9 @@
 package com.pattern.策略;
 
+/**
+ * 接口 {@code Strategy} 和 {@code Context} 是重要组成。
+ * 上下文中的 {@method calRecharge} 负责即时转换策略
+ */
 public class Context {
  
     private Strategy strategy;
