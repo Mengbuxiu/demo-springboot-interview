375cfe1689d1574b12c858dfb15b783351c98d5f
Alin <15910344021@163.com>
2021-02-22 22:24:43 +0800

    格式化

--- src/main/java/com/pattern/策略/Factory.java
+++ src/main/java/com/pattern/策略/Factory.java
@@ -4,21 +4,26 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Factory {
- 
+
     private static Factory factory = new Factory();
-    private Factory(){
+
+    private Factory() {
     }
-    private static Map<Integer,Strategy> strategyMap = new HashMap<>();
-    static{
-       strategyMap.put(RechargeTypeEnum.E_BANK.value(), new StrategyEBank());
-       strategyMap.put(RechargeTypeEnum.BUSI_ACCOUNTS.value(), new StrategyBusiAcct());
-       strategyMap.put(RechargeTypeEnum.MOBILE.value(), new StrategyMobile());
-       strategyMap.put(RechargeTypeEnum.CARD_RECHARGE.value(), new StrategyCard());
+
+    private static Map<Integer, Strategy> strategyMap = new HashMap<>();
+
+    static {
+        strategyMap.put(RechargeTypeEnum.E_BANK.value(), new StrategyEBank());
+        strategyMap.put(RechargeTypeEnum.BUSI_ACCOUNTS.value(), new StrategyBusiAcct());
+        strategyMap.put(RechargeTypeEnum.MOBILE.value(), new StrategyMobile());
+        strategyMap.put(RechargeTypeEnum.CARD_RECHARGE.value(), new StrategyCard());
     }
-    public Strategy creator(Integer type){
-       return strategyMap.get(type);
+
+    public Strategy creator(Integer type) {
+        return strategyMap.get(type);
     }
-    public static Factory getInstance(){
-       return factory;
+
+    public static Factory getInstance() {
+        return factory;
     }
 } 
