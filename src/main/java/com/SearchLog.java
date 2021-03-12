2336bcb67f74fb3c3f75e94130f9236e458448be
zhenglin <linzhenglin_v@xiaojuchefu.com>
2021-03-12 11:57:00 +0800

    URL 解析 url地址

--- src/main/java/com/SearchLog.java
+++ src/main/java/com/SearchLog.java
@@ -4,6 +4,7 @@ import com.alibaba.fastjson.JSONObject;
 import org.springframework.util.StringUtils;
 
 import java.io.*;
+import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.Map;
@@ -11,26 +12,25 @@ import java.util.Map;
 /**
  * @author zhenglin
  *  
- * @desc
- * // 评测题目: 
+ * @desc // 评测题目:
  * // 编写程序，从本机的指定日志文件中读取日志，
  * //过滤出其中logType为A，且content中url对应的值的host为"www.taobao.com"的记录，
  * //然后对content中的count求和，输出最终结果。
  * // 日志位置: /home/admin/test.log
  * // 日志格式: logType^Acontent, 其中"^A"为不可见字符，对应ASCII码为001。
  * //如果读取到某行不符合该格式，则忽略此行记录。
- *
+ * <p>
  * // 假定以下条件作为前提：
  * // 1. 程序对/home/admin/test.log有读权限
  * // 2. 当该行的logType=A时，content为json格式，当content中无法取得count时，认为count=0
- *
-   举例，/home/admin/test.log 文件内容如下：
-   A^A{"url":"http:www.baidu.com", "count": "1"}
-   B^A{"count": 1, "other": ""}
-   A^A{"url":"https:www.taobao.com", "count": "1"}
-   A^A{"url":"http:www.taobao.com"}
+ * <p>
+ * 举例，/home/admin/test.log 文件内容如下：
+ * A^A{"url":"http://www.baidu.com", "count": "1"}
+ * B^A{"count": 1, "other": ""}
+ * A^A{"url":"https://www.taobao.com?a=1&b=2", "count": "1"}
+ * A^A{"url":"http://www.taobao.com"}
  * // 执行完成后输出1。
- *
+ * <p>
  * // 注意考虑日志和url形式的多样性
  * @date 2021/3/11 下午7:18
  */
@@ -56,9 +56,12 @@ public class SearchLog {
                     String json = lineTxt.substring(3);
                     JSONObject jsonObject = JSONObject.parseObject(json);
                     String url = jsonObject.getString("url");
+                    URL parsedUrl = new URL(url);
+                    System.out.println(parsedUrl.getHost() + " : " + parsedUrl.getFile()
+                            + " : " + parsedUrl.getPort() + " : " + parsedUrl.getDefaultPort());
                     String count = jsonObject.getString("count");
                     if (StringUtils.hasText(url)
-                            && url.endsWith("www.taobao.com")
+                            && url.contains("www.taobao.com")
                             && StringUtils.hasText(count)) {
                         line++;
                     }
@@ -72,17 +75,14 @@ public class SearchLog {
     }
 
 
-    public static String stringToAscii(String value)
-    {
-        StringBuffer sbu = new StringBuffer();
+    public static String stringToAscii(String value) {
+        StringBuilder sbu = new StringBuilder();
         char[] chars = value.toCharArray();
         for (int i = 0; i < chars.length; i++) {
-            if(i != chars.length - 1)
-            {
-                sbu.append((int)chars[i]).append(",");
-            }
-            else {
-                sbu.append((int)chars[i]);
+            if (i != chars.length - 1) {
+                sbu.append((int) chars[i]);
+            } else {
+                sbu.append((int) chars[i]);
             }
         }
         return sbu.toString();
